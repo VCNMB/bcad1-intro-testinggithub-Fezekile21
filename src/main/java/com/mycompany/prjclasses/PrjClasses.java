7a9f1fc3055577d2b9d73f3751aec423f6bfd6f7
Fezekile21 <ST10093560@RCCONNECT.EDU.ZA>
2025-03-11 10:41:45 +0200

    initial commit
    added files etc

---
+++ src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class PrjClasses {
+
+    public static void main(String[] args) {
+        
+        newClass nc = new newClass(); //craeting an onbject of new class
+       
+        nc.helloClass(); // have to use the variable name nc to communicate with the class
+        
+        System.out.println(nc.strSecondClassVar);  // output the variable from the second class
+    }
+}
